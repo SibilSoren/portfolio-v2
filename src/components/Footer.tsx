835963af86f97195fbcf6b02206f6590a992fcbe
Sibil Sarjam Soren <sibil.sorenofficial@gmail.com>
2024-10-21 15:27:07 +0530

    credits modified

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,12 @@
-import React from 'react';
+import React from "react";
 
 const Footer: React.FC = () => {
   return (
     <footer className=" text-gray-300 py-6 mt-12">
       <div className="container mx-auto px-4">
         <div className="flex flex-col items-end justify-center">
-          <p className="text-sm">
-            Designed and built by Oskar Frantti Glen
-          </p>
+          <p className="text-sm">Made with by ❤️ Sibil Sarjam Soren</p>
+          <p className="text-xs">Design by Oskar Frantti Glen</p>
           <p className="text-xs mt-2">
             © {new Date().getFullYear()} All rights reserved
           </p>
